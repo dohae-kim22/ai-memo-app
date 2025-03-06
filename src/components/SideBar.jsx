d8641fec7d82dd8d02ed247956961710a509c323
DODO <dohae.kim22@gmail.com>
2025-03-06 12:04:27 +0100

    Add Logo Redirection

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,7 +7,6 @@ import { format } from "date-fns";
 const SideBar = () => {
   const memo = useSelector((state) => state.memo);
   const dispatch = useDispatch();
-  console.log(memo);
 
   const navigate = useNavigate();
 
@@ -28,7 +27,12 @@ const SideBar = () => {
 
   return (
     <div className="flex flex-col bg-slate-800 w-[200px] text-center">
-      <h1 className="font-bold text-3xl py-5">Memo AI</h1>
+      <h1
+        onClick={() => navigate("/")}
+        className="font-bold text-3xl py-5 cursor-pointer"
+      >
+        Memo AI
+      </h1>
       <button
         onClick={handleClickAdd}
         className="font-bold bg-white hover:bg-slate-700 text-slate-800 hover:text-white mx-auto my-4 w-50 px-5 py-1 rounded-lg block"
